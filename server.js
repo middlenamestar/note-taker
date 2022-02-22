04790b64f3c19f4e7bf9ea5cb97a85fcd95b0d35
priscilla ma <priscillama621@gmail.com>
2022-02-22 17:42:44 -0500

    changed port, added json middleware

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/index');
 
-const PORT = 3001;
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static('public'));
 
+// MIDDLEWARE. PARSE JSON AND URLENCODED FORM DATA.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
+
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
